8779f4d1da47accfb712425f1aa6d59dbde7ea06
Kalana <kalana.16@cse.mrt.ac.lk>
2019-01-24 14:53:19 +0530

    returns valid boolean

---
+++ model.py
@@ -0,0 +1,54 @@
+import keras
+import numpy as np
+from keras.models import Model
+from keras_vggface.vggface import VGGFace
+from keras_preprocessing.image import load_img,img_to_array
+from keras.applications.vgg16 import preprocess_input
+vggface = VGGFace(model='vgg16')
+
+vgg_face_descriptor = Model (inputs = vggface.layers[0].input , outputs = vggface.layers[-2].output)
+
+
+
+def preprocess_image(image_path):
+    img = load_img(image_path,target_size=(224,224))
+    img = img_to_array(img)
+    img = np.expand_dims(img,axis =0)
+    img = preprocess_input(img)
+    return img
+
+def findCosDistance(src_rep,test_rep):
+    a = np.matmul(np.transpose(src_rep),test_rep)
+    b=np.sum(np.multiply(src_rep,src_rep))
+    c= np.sum(np.multiply(test_rep,test_rep))
+    cosDist =  1- (a/(np.sqrt(b)*np.sqrt(c)))
+    print (cosDist)
+    return cosDist
+
+def findEucDistance(src_rep,test_rep):
+    euc_dist = src_rep - test_rep
+    euc_dist = np.sum(np.multiply(euc_dist,euc_dist))
+    euc_dist = np.sqrt(euc_dist)
+    return euc_dist
+
+
+epsilon = 0.40 #cosine similarity
+#epsilon = 120 #euclidean distance
+
+def verifyFace(img1,img2):
+    print('trying to verify')
+    img1_rep = vgg_face_descriptor.predict(preprocess_image(img1))[0,:]
+    img2_rep = vgg_face_descriptor.predict(preprocess_image(img2))[0,:]
+
+    cos_similarity = findCosDistance(img1_rep,img2_rep)
+    if(cos_similarity < epsilon):
+        print("verified... they are same person")
+    else:
+        print("unverified! they are not same person!")
+
+verifyFace('F:/sem5-mine/SE_project/Tutorials/deepface-sefiks/8.jpg','F:/sem5-mine/SE_project/Tutorials/deepface-sefiks/7.jpg')
+
+
+
+
+
---
+++ test2.py
@@ -0,0 +1,25 @@
+
+
+from flask import Flask, render_template, request,jsonify
+from werkzeug import secure_filename
+from model_copy2 import verifyUploadedFace,preprocess_image,findCosDistance
+app = Flask(__name__)
+
+
+print ("working")
+@app.route('/test', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      print ("post req")
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      if (verifyUploadedFace(secure_filename(f.filename))):
+         return jsonify(valid=True)
+      else:
+         return jsonify(valid=False)
+   else:
+      print ("no request")
+      return jsonify(valid=False)
+		
+if __name__ == '__main__':
+   app.run(debug = True)
